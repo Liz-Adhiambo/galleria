1ddbe912f35e17daf68bfa083e3f31ca64aa9400
Elizabeth Adhiambo <adhiamboliz3@gmail.com>
2022-05-28 20:34:18 +0300

    Added image model to models.py for displaying images

--- lizgalleria/models.py
+++ lizgalleria/models.py
@@ -1,5 +1,6 @@
-from django.db import models
 
+from django.db import models
+from django_resized import ResizedImageField
 
 class Location(models.Model):
     name = models.CharField(max_length =50)
@@ -49,3 +50,50 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+class Image(models.Model):
+    name = models.CharField(max_length = 60)
+    pic = models.ImageField(upload_to = 'uploads/')
+
+    landImage = ResizedImageField(size=[2878, 1618], crop=['middle', 'center'], default='default_land.jpg', upload_to='landscape')
+    tallImage = ResizedImageField(size=[1618, 2878], crop=['middle', 'center'], default='default_tall.jpg', upload_to='tall')
+    
+    description = models.TextField()
+    image_location = models.ForeignKey('Location',null=True, blank=True, on_delete=models.CASCADE)
+    image_category = models.ForeignKey('Category',null=True, blank=True, on_delete=models.CASCADE)
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    @classmethod
+    def update_image(cls, id ,name, description , image_location, image_category):
+        update = cls.objects.filter(id = id).update(name = name, description = description ,image_location = image_location,image_category = image_category)
+        # return update
+
+    @classmethod
+    def get_all_images(cls):
+        images = cls.objects.all()
+        return images
+
+    @classmethod
+    def get_image_by_id(cls,id):
+        image = cls.objects.filter(id= id).all()
+        return image
+
+    @classmethod
+    def search_by_category(cls,image_category):
+        images = Image.objects.filter(image_category__name__icontains=image_category)
+        return images
+
+    @classmethod
+    def filter_by_location(cls, image_location):
+        images_location = cls.objects.filter(image_location__id=image_location)
+        return images_location
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
