a97cdbf1a1c6a9de6b4f591cf6951591ab22d27c
Elizabeth Adhiambo <adhiamboliz3@gmail.com>
2022-05-28 19:37:57 +0300

    Added Location model and made first migration

--- lizgalleria/models.py
+++ lizgalleria/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Location(models.Model):
+    name = models.CharField(max_length =50)
+
+    @classmethod
+    def tag_articles(cls):
+        tags = cls.objects.all()
+        return tags
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
+
+    def update_location(self, update):
+        self.name = update
+        self.save()
+
+    @classmethod
+    def get_location_id(cls, id):
+        locate = Location.objects.get(pk = id)
+        return locate
+
+    def __str__(self):
+        return self.name
+
--- lizgalleria/tests.py
+++ lizgalleria/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from django.test import TestCase
 
+from .models import Location
 # Create your tests here.
+
+class LocationTestCLass(TestCase):
+    #Set up Method
+    def setUp(self):
+        self.loc = Location(name="Africa")
+        self.loc.save_location()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.loc,Location))
+
+    def test_save_method(self):
+        self.loc.save_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations) > 0)
+
+    def test_delete_method(self):
+        self.loc.save_location()
+        self.loc.delete_location()
+        location = Location.objects.all()
+        self.assertTrue(len(location) == 0)
+
+    def test_update(self):
+        location = Location.get_location_id(self.loc.id)
+        location.update_location('Juja')
+        location = Location.get_location_id(self.loc.id)
+        self.assertTrue(location.name == 'Juja')
+
+
