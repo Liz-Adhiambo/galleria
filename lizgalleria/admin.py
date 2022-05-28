17932a288367b97f61304ac9542a7ca3aa69eb14
Elizabeth Adhiambo <adhiamboliz3@gmail.com>
2022-05-28 20:36:19 +0300

    Added image model to admin.py file to add Images through the admin

--- lizgalleria/admin.py
+++ lizgalleria/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from lizgalleria.models import Category, Location
+from lizgalleria.models import Category, Location,Image
 
 # Register your models here.
 admin.site.register(Location)
 admin.site.register(Category)
+admin.site.register(Image)
