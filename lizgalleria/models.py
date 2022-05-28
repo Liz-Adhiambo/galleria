9d7e50d3d62469109585862fe7340d8d9fbfd7da
Elizabeth Adhiambo <adhiamboliz3@gmail.com>
2022-05-28 19:40:55 +0300

    Added Category model for image categories

--- lizgalleria/models.py
+++ lizgalleria/models.py
@@ -27,3 +27,25 @@ class Location(models.Model):
     def __str__(self):
         return self.name
 
+class Category(models.Model):
+    name = models.CharField(max_length =50)
+
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+
+    def update_category(self, update):
+        self.name = update
+        self.save()
+
+    @classmethod
+    def get_category_id(cls, id):
+        category = Category.objects.get(pk = id)
+        return category
+
+    def __str__(self):
+        return self.name
+
