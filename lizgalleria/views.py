c8c14fc6d73ad9f39a96b68f29e84722ae3be8c4
Elizabeth Adhiambo <adhiamboliz3@gmail.com>
2022-05-28 20:48:56 +0300

    Included app urls in the project url file

--- lizgalleria/views.py
+++ lizgalleria/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from .models import *
+
+
+def home(request):
+    return render(request, 'landing.html')
 
 # Create your views here.
