084af1bbde6455a31676f2c42e06e2a7ae39a16a
Elizabeth Adhiambo <adhiamboliz3@gmail.com>
2022-05-31 10:23:05 +0300

    Added remaining Image tests

--- gallery/settings.py
+++ gallery/settings.py
@@ -78,41 +78,41 @@ WSGI_APPLICATION = 'gallery.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'galleria',
-#         'USER': 'liz',
-#     'PASSWORD':'1234',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'galleria',
+        'USER': 'liz',
+    'PASSWORD':'1234',
+    }
+}
 
-if config('MODE')=="dev":
-   DATABASES = {
-       'default': {
-           'ENGINE': 'django.db.backends.postgresql_psycopg2',
-           'NAME': config('DB_NAME'),
-           'USER': config('DB_USER'),
-           'PASSWORD': config('DB_PASSWORD'),
-           'HOST': config('DB_HOST'),
-           'PORT': '',
-       }
+# if config('MODE')=="dev":
+#    DATABASES = {
+#        'default': {
+#            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#            'NAME': config('DB_NAME'),
+#            'USER': config('DB_USER'),
+#            'PASSWORD': config('DB_PASSWORD'),
+#            'HOST': config('DB_HOST'),
+#            'PORT': '',
+#        }
        
-   }
-# production
-else:
-   DATABASES = {
-       'default': dj_database_url.config(
-           default=config('DATABASE_URL')
-       )
-   }
+#    }
+# # production
+# else:
+#    DATABASES = {
+#        'default': dj_database_url.config(
+#            default=config('DATABASE_URL')
+#        )
+#    }
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+# ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
 
 
 
--- lizgalleria/tests.py
+++ lizgalleria/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.test import TestCase
 
-from .models import Location
+from .models import Category, Image, Location
 # Create your tests here.
 
 class LocationTestCLass(TestCase):
@@ -29,5 +29,79 @@ class LocationTestCLass(TestCase):
         location.update_location('Juja')
         location = Location.get_location_id(self.loc.id)
         self.assertTrue(location.name == 'Juja')
+ #test for Category Model
+
+class CategoryTestClass(TestCase):
+    # Set up Method
+    def setUp(self):
+        self.cat = Category(name="fashion")
+        self.cat.save_category()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.cat, Category))
+
+    def test_save_method(self):
+        self.cat.save_category()
+        category = Category.objects.all()
+        self.assertTrue(len(category) > 0)
+
+    def test_delete_method(self):
+        self.cat.save_category()
+        self.cat.delete_category()
+        category = Category.objects.all()
+        self.assertTrue(len(category) == 0)
+
+    def test_update(self):
+        category = Category.get_category_id(self.cat.id)
+        category.update_category('Travel')
+        category = Category.get_category_id(self.cat.id)
+        self.assertTrue(category.name == 'Travel')
+
+ #test for Image Model
+
+class ImageTestClass(TestCase):
+    # Set up Method
+    def setUp(self):
+        self.cat = Category(name="fashion")
+        self.cat.save_category()
+
+        self.loc = Location(name="Africa")
+        self.loc.save_location()
+
+        self.image = Image(name='image test', description='my test',image_location=self.loc, image_category=self.cat)
+        self.image.save_image()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image, Image))
+
+    def tearDown(self):
+        self.image.delete_image()
+        self.cat.delete_category()
+        self.loc.delete_location()
 
 
+    def test_save_method(self):
+        self.image.save_image()
+        images  = Image.objects.all()
+        self.assertTrue(len(images)>0)
+
+
+    def test_get_all_images(self):
+        images = Image.get_all_images()
+        self.assertTrue(len(images)>0)
+
+    def test_get_image_by_id(self):
+        images= Image.get_image_by_id(self.image.id)
+        self.assertTrue(len(images) == 1)
+
+    def test_search_by_category(self):
+        images = Image.search_by_category('fashion')
+        self.assertTrue(len(images)>0)
+
+
+    def test_update_image(self):
+        self.image.save_image()
+        image = Image.update_image( self.image.id, 'test update', 'my test',self.loc, self.cat)
+        upimage = Image.objects.filter(id = self.image.id)
+        print(upimage)
+        self.assertTrue(Image.name == 'test update')
