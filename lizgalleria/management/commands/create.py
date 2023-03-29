2be2a33831e3b29fd322678412bc9faa478c572c
Elizabeth Adhiambo <adhiamboliz3@gmail.com>
2023-03-29 13:48:23 +0300

    Updated dep pro

--- lizgalleria/management/commands/create.py
+++ lizgalleria/management/commands/create.py
@@ -1,15 +1,17 @@
 
-from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
-
+from django.contrib.auth import get_user_model
 
 class Command(BaseCommand):
-    help = 'Creates a superuser.'
+    help = 'Create a superuser account'
 
     def handle(self, *args, **options):
-        if not User.objects.filter(username='liz').exists():
-            User.objects.create_superuser(
-                username='liz',
-                password='testing1235'
-            )
-        print('Superuser has been created.')
+        User = get_user_model()
+        username = 'admin'
+        email = 'admin@example.com'
+        password = 'admin'
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(username=username, email=email, password=password)
+            self.stdout.write(self.style.SUCCESS('Superuser created successfully'))
+        else:
+            self.stdout.write(self.style.WARNING('Superuser already exists'))
